e3de68bf74e1e4295ea5bf7d337d339b2e81d16f
Yuki-han <1047241339@qq.com>
2021-08-05 17:43:12 +0800

    添加cesium的入口文件中配置

--- src/pages/MapMain/index.jsx
+++ src/pages/MapMain/index.jsx
@@ -1,10 +1,20 @@
 import React, { useEffect } from 'react';
 import { connect } from 'umi';
 
+// cesium配置 - 入口文件中配置
+// 导入必须的样式表
+import 'cesium/Source/Widgets/widgets.css';
+// 我们刚才所说的如何让Cesium知道静态资源在哪里的API
+import buildModuleUrl from 'cesium/Source/Core/buildModuleUrl';
+
 import MapHeader from '@/pages/MapHeader';
 import MapView from '@/pages/MapView';
 import MapTool from '@/pages/MapTool';
 import MapControl from '@/pages/MapControl';
+
+// cesium配置 - 入口文件中配置
+// 设置静态资源目录
+buildModuleUrl.setBaseUrl('../cesium');
 
 const MapMain = () => {
   return (
